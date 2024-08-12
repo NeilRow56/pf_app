c01b82e2c9f73ddb63ee43972f7c20443e861b35
NeilRow56 <neilrowland56@gmail.com>
2024-08-12 10:06:55 +0100

    Initial Dashboard layout and Navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,6 +14,7 @@ import { DashboardNavigation } from "@/components/dashboard/DashboardNavigation"
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import { ThemeToggle } from "@/components/dashboard/ThemeToggle";
 
 export default async function DashboardLayout({
   children,
@@ -28,43 +29,46 @@ export default async function DashboardLayout({
   }
   return (
     <div className="flex flex-col w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
+      <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b ">
         <nav className="hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <DashboardNavigation />
         </nav>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              className="shrink-0 md:hidden"
-              variant="outline"
-              size="icon"
-            >
-              <MenuIcon className="h-5 w-5" />
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left">
-            <nav className="flex flex-col gap-6 text-lg font-medium mt-5">
-              <DashboardNavigation />
-            </nav>
-          </SheetContent>
-        </Sheet>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="secondary" size="icon" className="rounded-full">
-              <CircleUser className="w-5 h-5" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuItem asChild>
-              <LogoutLink>{user.email}</LogoutLink>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <LogoutLink>Logout</LogoutLink>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="flex items-center gap-4">
+          <ThemeToggle />
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                className="shrink-0 md:hidden"
+                variant="outline"
+                size="icon"
+              >
+                <MenuIcon className="h-5 w-5" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <nav className="flex flex-col gap-6 text-lg font-medium mt-5">
+                <DashboardNavigation />
+              </nav>
+            </SheetContent>
+          </Sheet>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="w-5 h-5" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuItem asChild>
+                <LogoutLink>{user.email}</LogoutLink>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <LogoutLink>Logout</LogoutLink>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </header>
 
       <main className="my-5">{children}</main>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import Big from "big.js";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const MAX_VALUE = 9999999.99;
+
+const currencyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+export const formatCurrency = (value: number) => {
+  return currencyFormatter.format(value);
+};
+
+export const toCents = (amount: number): number =>
+  new Big(amount).times(100).toNumber();
+
+export const convertCurrencyToNumber = (amount: string): number =>
+  parseFloat(amount.replace(",", "."));
+export const isAmountWithinRange = (amount: number): boolean =>
+  MAX_VALUE * -1 <= amount && amount <= MAX_VALUE;
